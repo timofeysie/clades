898bae2af432901e9388aa38a3a18578b0c769d1
timofeysie <timofeyc@hotmail.com>
2020-04-15 14:34:44 +1000

    closes #2 added tests for the reducers

--- apps/stratum/src/app/counter/counter.component.spec.ts
+++ apps/stratum/src/app/counter/counter.component.spec.ts
@@ -2,12 +2,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CounterComponent } from './counter.component';
 import { Store, StoreModule } from '@ngrx/store';
 import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { createAction } from '@ngrx/store';
+import { counterReducer, initialState } from '../store/counter.reducer';
+import { increment, decrement, reset } from '../store/counter.actions';
 
 describe('CounterComponent', () => {
   let component: CounterComponent;
   let fixture: ComponentFixture<CounterComponent>;
   let store: MockStore;
-  const initialState = { count: 0 };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -29,12 +31,25 @@ describe('CounterComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('counter should start at 0',  done => {
-    component.count$.subscribe( count => {
-      expect(count).toEqual(0);
-      done();
-    });
-    
+  it('should return a default state using nonexistent action', () => {
+    const noopAction = createAction('noop');
+    const newState = counterReducer(0, noopAction);
+    expect(newState).toEqual(initialState);
   });
 
+  it('increment reducer should increment the state', () => {
+    const incrementedState = counterReducer(0, increment);
+    expect(incrementedState).toBe(1);
+  });
+
+  it('decrement reducer should decrement the state', () => {
+    const decrementedState = counterReducer(0, decrement);
+    expect(decrementedState).toBe(-1);
+  });
+
+  it('reset reducer should reset the state', () => {
+    const resetState = counterReducer(2, reset);
+    expect(resetState).toBe(0);
+  });
+  
 });
